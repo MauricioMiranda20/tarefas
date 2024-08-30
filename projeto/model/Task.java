fa275721e29b0fea5d3cc334676fb871d16356eb
Mauricio Miranda <64110966+MauricioMiranda20@users.noreply.github.com>
2024-08-29 23:43:35 -0300

    Update Task.java

--- projeto/model/Task.java
+++ projeto/model/Task.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+
 
 @Entity
 public class Task {
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
+	@Getter//esse é usado pelo lombok para user get
 	private long id;
 	private String title;
 	private String description;
@@ -23,30 +26,5 @@ public class Task {
 		this.title = title;
 		this.description = description;
 		this.completed = completed;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public boolean isCompleted() {
-		return completed;
-	}
-	public void setCompleted(boolean completed) {
-		this.completed = completed;
-	}
-	
+	}	
 }
